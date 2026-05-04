31139b7882773a2217ba9eba89572035d6b88076
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-04 22:34:31 +0200

    type fix

--- src/app/objects/page.tsx
+++ src/app/objects/page.tsx
@@ -36,7 +36,7 @@ const Objects = () => {
         name: "John",
         age: 20,
         class : "FED",
-        
+        printName: () => "John"
     }
 
  type classRep = student & {
@@ -46,7 +46,8 @@ const Objects = () => {
         name: "Jane",
         age: 22,
         class:"FED",
-        phone: 1234567890
+        phone: 1234567890,
+        printName: () => "John"
     }
     const Bob: classRep = {...John, phone: 987654321}
 

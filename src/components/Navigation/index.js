acda831eb28e66873506e1f96d708132e9bae2d1
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-04 22:27:27 +0200

    create develop branch

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import styles from "./navigation.module.css";
 
-
-
 const Navigation = () => {
 
     const navItems = ["Home", "Arrays", "Primitives", "Objects", "Functions", "Props", "Fetch"];
